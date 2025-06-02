6df93e68de12472ca16d0ae409a3af7786429121
Parth3105 <123438526+Parth3105@users.noreply.github.com>
2025-06-02 12:24:29 -0700

    Handling of the Uploaded files (#3)
    * Handling of the uploaded files

---
+++ src/main/java/in/ac/daiict/deep/service/UploadService.java
@@ -0,0 +1,11 @@
+package in.ac.daiict.deep.service;
+
+import in.ac.daiict.deep.entity.Upload;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UploadService {
+    public void insertAll(Map<String,Upload> uploads);
+    public List<Upload> getAll();
+}
---
+++ src/main/java/in/ac/daiict/deep/service/impl/UploadServiceImpl.java
@@ -0,0 +1,28 @@
+package in.ac.daiict.deep.service.impl;
+
+import in.ac.daiict.deep.entity.Upload;
+import in.ac.daiict.deep.repository.UploadRepo;
+import in.ac.daiict.deep.service.UploadService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class UploadServiceImpl implements UploadService {
+    private UploadRepo uploadRepo;
+
+    @Override
+    public void insertAll(Map<String, Upload> uploads) {
+        List<Upload> uploadList = new ArrayList<>(uploads.values());
+        uploadRepo.saveAllAndFlush(uploadList);
+    }
+
+    @Override
+    public List<Upload> getAll() {
+        return List.of();
+    }
+}
