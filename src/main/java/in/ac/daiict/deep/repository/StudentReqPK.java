d1fd175eee05681b4e14ee7fadb4befe4b155fa5
Parth3105 <parthvadodaria17@gmail.com>
2025-06-05 17:03:41 +0530

    Implemented the functionality to send the available course

--- src/main/java/in/ac/daiict/deep/repository/StudentReqPK.java
+++
@@ -1,7 +0,0 @@
-package in.ac.daiict.deep.repository;
-
-import in.ac.daiict.deep.entity.StudentReq;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentReqPK extends JpaRepository<StudentReq, StudentReqPK> {
-}
