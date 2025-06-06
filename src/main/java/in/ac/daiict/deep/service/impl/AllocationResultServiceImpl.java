206de8b6161fb3635dbb25b61e1bad7aa13ecd13
Parth3105 <123438526+Parth3105@users.noreply.github.com>
2025-06-06 07:07:02 -0700

    Enhanced ResultDisplayController (#21)
    * Enhanced the result displaying controller to display the allocation result of the student

--- src/main/java/in/ac/daiict/deep/service/impl/AllocationResultServiceImpl.java
+++ src/main/java/in/ac/daiict/deep/service/impl/AllocationResultServiceImpl.java
@@ -1,5 +1,6 @@
 package in.ac.daiict.deep.service.impl;
 
+import in.ac.daiict.deep.dto.AllocationResultDto;
 import in.ac.daiict.deep.entity.AllocationResult;
 import in.ac.daiict.deep.repository.AllocationResultRepo;
 import in.ac.daiict.deep.service.AllocationResultService;
@@ -16,5 +17,12 @@ public class AllocationResultServiceImpl implements AllocationResultService {
     @Override
     public void insertAll(List<AllocationResult> allocationResultList) {
         allocationResultRepo.saveAll(allocationResultList);
+    }
+
+    @Override
+    public List<AllocationResultDto> fetchAllocationResult(String sid, String program) {
+        List<AllocationResultDto> allocationResultDtoList=allocationResultRepo.fetchAllocationResultBySid(sid, program);
+        if(allocationResultDtoList==null || allocationResultDtoList.isEmpty()) return null;
+        return allocationResultDtoList;
     }
 }
