03e36a074232eb43ff145effa2d1830889cdfb7b
Parth3105 <parthvadodaria17@gmail.com>
2025-06-07 16:05:16 +0530

    Implemented functionality to create a sheet for each course containing registered student data

--- src/main/java/in/ac/daiict/deep/service/AllocationReportService.java
+++
@@ -1,8 +0,0 @@
-package in.ac.daiict.deep.service;
-
-import in.ac.daiict.deep.entity.AllocationReport;
-
-public interface AllocationReportService {
-    public void insertReport(AllocationReport allocationReport);
-    public void deleteReport(String fileName);
-}
