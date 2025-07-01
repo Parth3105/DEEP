5a72e10a8471ac820146963398ac1b8a5edf5262
Parth3105 <123438526+Parth3105@users.noreply.github.com>
2025-07-01 01:09:56 -0700

    Minor changes as per requested requirements (#51)
    * minor changes as requested

--- src/main/java/in/ac/daiict/deep/constant/endpoints/StudentEndpoint.java
+++ src/main/java/in/ac/daiict/deep/constant/endpoints/StudentEndpoint.java
@@ -3,7 +3,7 @@ package in.ac.daiict.deep.constant.endpoints;
 public class StudentEndpoint {
     public static final String STUDENT_BASE="/student";
     public static final String HOME_PAGE=STUDENT_BASE+"/home-page";
-    public static final String ENROLL=STUDENT_BASE+"/enroll";
+    public static final String PREFERENCE_FORM =STUDENT_BASE+"/preference-form";
     public static final String SUBMIT_PREFERENCE=STUDENT_BASE+"/submit-preferences";
     public static final String PREFERENCE_SUMMARY=STUDENT_BASE+"/my-preferences";
     public static final String ALLOCATION_RESULT=STUDENT_BASE+"/my-allocation-result";
