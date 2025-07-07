96d87e4c987626e2c78f8f51df48ec36d30d4e26
Parth3105 <parthvadodaria17@gmail.com>
2025-07-07 14:59:05 +0530

    Enhanced functionality for instance creation and saving data of previous instance

--- src/main/java/in/ac/daiict/deep/service/UserService.java
+++ src/main/java/in/ac/daiict/deep/service/UserService.java
@@ -3,8 +3,11 @@ package in.ac.daiict.deep.service;
 import in.ac.daiict.deep.entity.User;
 import in.ac.daiict.deep.dto.ResponseDto;
 
+import java.io.File;
+
 public interface UserService {
     User findUser(String username);
     ResponseDto resetPassword(String username, String password);
-    boolean migrateUserData();
+    void migrateUserData(File dir);
+    boolean insertFromFile(File dir);
 }
