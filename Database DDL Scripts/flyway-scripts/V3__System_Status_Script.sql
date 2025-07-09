7e29790296680b1e38d482e77fe34dbe0ec22160
Parth3105 <123438526+Parth3105@users.noreply.github.com>
2025-07-09 07:58:45 -0700

    Included DDL Scripts (#59)
    * removed unncessary files
    
    * Included the Database DDL Scripts

---
+++ Database DDL Scripts/flyway-scripts/V3__System_Status_Script.sql
@@ -0,0 +1,3 @@
+INSERT INTO system_status VALUES ('registration_status', 'close');
+INSERT INTO system_status VALUES ('update_instance_status', 'open');
+INSERT INTO system_status VALUES ('result_status', 'pending');
